92ea85c20dc1f5f4236c729a17fa34a9ed9718d9
Djcharles26 <jlaraanaya@gmail.com>
2020-12-07 13:02:48 -0600

    Refactor uuid.v4 function for v4() in alertActions

--- src/actions/alertsActions.js
+++ src/actions/alertsActions.js
@@ -1,10 +1,10 @@
-import { v4 as uuidv4 } from 'uuid';
+import {v4} from 'uuid';
 
 import { SET_ALERT, REMOVE_ALERT } from './types';
 
 export const alert_set = (msg, alert_type, timeout = 5000) => dispatch => {
 
-    const id = uuidv4.v4 ();
+    const id = v4 ();
     dispatch ({
         type: SET_ALERT,
         payload: { msg, alert_type, id }
