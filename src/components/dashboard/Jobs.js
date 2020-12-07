4c71ff461cc08eb0d72a276e1239172684e214f3
Luis Huerta <luisgerhuerta@gmail.com>
2020-12-07 00:03:56 -0600

    Added base UI for jobs

--- src/components/dashboard/Jobs.js
+++ src/components/dashboard/Jobs.js
@@ -10,6 +10,9 @@ import Footer from '../main/Footer';
 
 import Spinner from '../common/Spinner';
 
+import TextField from '../input/TextField';
+import TextArea from '../input/TextArea';
+
 import { jobs_get_all } from '../../actions/jobsActions';
 
 class Jobs extends Component {
@@ -40,8 +43,111 @@ class Jobs extends Component {
         <div>
             <Navbar />
 
-
-
+            <div style={{display: "flex", justifyContent: "center", marginTop: "1%", marginBottom: "1%"}}>
+                <div className="card" style={{width: "65%"}}>
+                    <div className="card-body">
+                        <h2 className="card-title">Create</h2>
+                        <form>
+                            <TextField 
+                                placeholder="Name"
+                                name="name"
+                            />
+                            <TextArea
+                                placeholder="Description"
+                                name="description"
+                            />
+                            <button type="submit" className="m-btn m-btn-theme m-btn-radius btn-lg w-100"><i className="fas fa-plus"></i> Create</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div style={{display: "flex", justifyContent: "center", marginTop: "1%", marginBottom: "1%"}}>
+                <div className="card" style={{width: "65%"}}>
+                    <div className="card-body">
+                        <h2 className="card-title">Waiting</h2>
+                        <table className="table">
+                            <thead style={{backgroundColor: "#024a75", color: "#ffffff"}}>
+                                <tr>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Created</th>
+                                    <th scope="col">Images</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">Nombre1</th>
+                                    <td>Fecha Inicio</td>
+                                    <td>1 / 10</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Nombre2</th>
+                                    <td>Fecha Inicio</td>
+                                    <td>2 / 10</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <div style={{display: "flex", justifyContent: "center", marginTop: "1%", marginBottom: "1%"}}>
+                <div className="card" style={{width: "65%"}}>
+                    <div className="card-body">
+                        <h2 className="card-title">Current</h2>
+                        <table className="table">
+                            <thead style={{backgroundColor: "#024a75", color: "#ffffff"}}>
+                                <tr>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Started</th>
+                                    <th scope="col">Images</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">Nombre1</th>
+                                    <td>Fecha Inicio</td>
+                                    <td>1 / 10</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Nombre2</th>
+                                    <td>Fecha Inicio</td>
+                                    <td>2 / 10</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <div style={{display: "flex", justifyContent: "center", marginTop: "1%", marginBottom: "3%"}}>
+                <div className="card" style={{width: "65%"}}>
+                    <div className="card-body">
+                        <h2 className="card-title">Finished</h2>
+                        <table className="table">
+                            <thead style={{backgroundColor: "#024a75", color: "#ffffff"}}>
+                                <tr>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Started</th>
+                                    <th scope="col">Finished</th>
+                                    <th scope="col">Images</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">Nombre1</th>
+                                    <td>Fecha Inicio</td>
+                                    <td>Fecha Fin</td>
+                                    <td>1 / 10</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Nombre2</th>
+                                    <td>Fecha Inicio</td>
+                                    <td>Fecha Fin</td>
+                                    <td>2 / 10</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
             <Footer />
         </div>
         );
