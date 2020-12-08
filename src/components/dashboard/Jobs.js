26c9a54071125fdd2024b066a28d0532e0a43a83
ermiry <erick_salas@ermiry.com>
2020-12-08 16:12:19 -0600

    Refactored jobs indentation

--- src/components/dashboard/Jobs.js
+++ src/components/dashboard/Jobs.js
@@ -17,247 +17,229 @@ import { jobs_get_all, jobs_create } from '../../actions/jobsActions';
 
 class Jobs extends Component {
 
-    constructor(props){
-        super(props);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-        this.state= {
-            name: "",
-            description: "",
-            creating: false
-        };
-        this.fetchJob = this.fetchJob.bind(this);
-    }
+	constructor(props){
+		super(props);
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+		this.state= {
+				name: "",
+				description: "",
+				creating: false
+		};
+		this.fetchJob = this.fetchJob.bind(this);
+	}
 
-    componentDidMount () {
-        // request for all jobs
-        this.props.jobs_get_all (); 
-    }
+	componentDidMount () {
+		// request for all jobs
+		this.props.jobs_get_all ();
+	}
 
-    handleSubmit(e){
-        e.preventDefault();
-        this.setState({creating: true});
+	handleSubmit(e){
+		e.preventDefault();
+		this.setState({creating: true});
 
-        this.props.jobs_create (this.state) ;
-    }
+		this.props.jobs_create (this.state) ;
+	}
 
-    handleChange(e) {
-        this.setState({ [e.target.name]: e.target.value })
-    }
+	handleChange(e) {
+		this.setState({ [e.target.name]: e.target.value })
+	}
 
-    fetchJob(job_id) {
-        window.location.href = "/jobs/" + job_id;
-    }
+	fetchJob(job_id) {
+		window.location.href = "/jobs/" + job_id;
+	}
 
-    render () {
-        const jobs = this.props.jobs.jobs;
-        const jobs_loading = this.props.jobs.loading;
-        
+	render () {
+		const jobs = this.props.jobs.jobs;
+		const jobs_loading = this.props.jobs.loading;
 
-        // TODO:
-        let content = <div className="container"><Spinner /></div>;
+		let content = <div className="container"><Spinner /></div>;
 
-        // if (users === null || user_loading) {
-        //     content = (
-        //         <div className="container-fluid"><Spinner /></div>
-        //     );
-        // } else {
-        //     content = (
-        //         <Users className="h-100" location={this.props.locations.highlight} user={this.props.auth.user} users={users} real_days={real_days}/>
-        //     );
-        // }
+		return (
+		<div>
+			<Navbar />
+			{/* Create div */}
+			<div style={{display: "flex", justifyContent: "center", marginTop: "1%", marginBottom: "1%"}}>
+				<div className="card" style={{width: "65%"}}>
+					<div className="card-body">
+						<h2 className="card-title">Create</h2>
+						<form onSubmit={this.handleSubmit}>
+							<TextField
+								icon=""
+								onChange={this.handleChange}
+								value={this.state.name}
+								placeholder="Name"
+								name="name"
+								/>
+							<TextArea
+								onChange={this.handleChange}
+								value={this.state.description}
+								placeholder="Description"
+								name="description"
+								/>
+							<button type="submit" className="m-btn m-btn-theme m-btn-radius btn-lg w-100">
+								Create
+							</button>
+						</form>
+					</div>
+				</div>
+			</div>
+			{jobs_loading || jobs === null
+				? content
+				:
+				<div>
+					{/* Waiting div */}
+					<div style={{display: "flex", justifyContent: "center", marginTop: "1%", marginBottom: "1%"}}>
+						<div className="card" style={{width: "65%"}}>
+							<div className="card-body">
+									<h2 className="card-title">Waiting</h2>
+									{jobs.filter((j) => j.status === 1 ).length > 0
+										? <table className="table text-center">
+											<thead style={{backgroundColor: "#024a75", color: "#ffffff"}}>
+												<tr>
+													<th scope="col">Name</th>
+													<th scope="col">Description</th>
+													<th scope="col">Created</th>
+												</tr>
+											</thead>
+												<tbody>
+													{jobs.filter((j) => j.status === 1 ).map((j) => {
+														return (
+														<tr onClick={() => this.fetchJob(j._id.$oid)}>
+															<th scope="row">{j.name}</th>
+															<td scope="row">{j.description}</td>
+															<td><Moment format="DD-MM-YYYY hh:mm:ss">{j.created.$date}</Moment></td>
+														</tr>
+													)})}
+												</tbody>
+										</table>
+										: <h4 style={{fontWeight:"500"}}>There are no waiting jobs yet, create one first.</h4>
+									}
+								</div>
+						</div>
+					</div>
 
-        return (
-        <div>
-            <Navbar />
-            {/* Create div */}
-            <div style={{display: "flex", justifyContent: "center", marginTop: "1%", marginBottom: "1%"}}>
-                <div className="card" style={{width: "65%"}}>
-                    <div className="card-body">
-                        <h2 className="card-title">Create</h2>
-                        <form onSubmit={this.handleSubmit}>
-                            <TextField
-                                icon=""
-                                onChange={this.handleChange} 
-                                value={this.state.name}
-                                placeholder="Name"
-                                name="name"
-                                />
-                            <TextArea
-                                onChange={this.handleChange} 
-                                value={this.state.description}
-                                placeholder="Description"
-                                name="description"
-                                />
-                            <button type="submit" className="m-btn m-btn-theme m-btn-radius btn-lg w-100"><i className="fas fa-plus"></i> Create</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            {jobs_loading || jobs === null
-                ? content 
-                : 
-                <div>
-                    {/* Waiting div */}
-                    <div style={{display: "flex", justifyContent: "center", marginTop: "1%", marginBottom: "1%"}}>
-                        <div className="card" style={{width: "65%"}}>
-                            <div className="card-body">
-                                <h2 className="card-title">Waiting</h2>
-                                {jobs.filter((j) => j.status === 1 ).length > 0  
-                                    ? <table className="table text-center">
-                                        <thead style={{backgroundColor: "#024a75", color: "#ffffff"}}>
-                                            <tr>
-                                                <th scope="col">Name</th>
-                                                <th scope="col">Description</th>
-                                                <th scope="col">Created</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            
-                                            {jobs.filter((j) => j.status === 1 ).map((j) => {
-                                                return (
-                                                <tr onClick={() => this.fetchJob(j._id.$oid)}>
-                                                    <th scope="row">{j.name}</th>
-                                                    <td scope="row">{j.description}</td>
-                                                    <td><Moment format="DD-MM-YYYY hh:mm:ss">{j.created.$date}</Moment></td>
-                                                </tr>
-                                            )})}
-                                                
-                                        </tbody>
-                                    </table>
-                                    : <h4 style={{fontWeight:"500"}}>There are no waiting jobs yet, create one first.</h4>
-                                }
-                                
-                            </div>
-                        </div>
-                    </div>
+					{/* Ready div */}
+					<div style={{display: "flex", justifyContent: "center", marginTop: "1%", marginBottom: "1%"}}>
+						<div className="card" style={{width: "65%"}}>
+							<div className="card-body">
+								<h2 className="card-title">Ready</h2>
+								{jobs.filter((j) => j.status === 2 ).length > 0
+									?
+									<table className="table text-center">
+										<thead style={{backgroundColor: "#024a75", color: "#ffffff"}}>
+											<tr>
+												<th scope="col">Name</th>
+												<th scope="col">Images</th>
+												<th scope="col">Type</th>
+											</tr>
+										</thead>
+										<tbody>
+											{
+												jobs.filter((j) => j.status === 2 ).map((j) => {
+													return (
+													<tr onClick={() => this.fetchJob(j._id.$oid)}>
+														<th scope="row">{j.name}</th>
+														<td scope="row">{j.imagesCount}</td>
+														<td>{j.type}</td>
+													</tr>
+												)})
+											}
+										</tbody>
+								</table>
+									: <h4 style={{fontWeight:"500"}}>There are no jobs ready yet</h4>
+								}
+							</div>
+						</div>
+					</div>
+					{/* Current */}
+					<div style={{display: "flex", justifyContent: "center", marginTop: "1%", marginBottom: "1%"}}>
+						<div className="card" style={{width: "65%"}}>
+							<div className="card-body">
+								<h2 className="card-title">Current</h2>
+								{jobs.filter((j) => j.status === 3 ).length > 0
+									?
+									<table className="table text-center">
+										<thead style={{backgroundColor: "#024a75", color: "#ffffff"}}>
+											<tr>
+												<th scope="col">Name</th>
+												<th scope="col">Started</th>
+												<th scope="col">Images</th>
+											</tr>
+										</thead>
+										<tbody>
+											{
+												jobs.filter((j) => j.status === 3 ).map((j) => {
+													return (
+													<tr onClick={() => this.fetchJob(j._id.$oid)}>
+														<th scope="row">{j.name}</th>
+														<td scope="row"><Moment format="DD-MM-YYYY hh:mm:ss">{j.started.$date}</Moment></td>
+														<td>{j.images.filter((i) => i.result !== "null").length}/{j.images.length}</td>
+													</tr>
+												)})
+											}
+										</tbody>
+									</table>
+									:<h4 style={{fontWeight:"500"}}>There are no jobs running yet</h4>
+								}
+							</div>
+						</div>
+					</div>
+					<div style={{display: "flex", justifyContent: "center", marginTop: "1%", marginBottom: "3%"}}>
+						<div className="card" style={{width: "65%"}}>
+							<div className="card-body">
+								<h2 className="card-title">Finished</h2>
+									{jobs.filter((j) => j.status === 6 ).length > 0
+									? <table className="table text-center">
+											<thead style={{backgroundColor: "#024a75", color: "#ffffff"}}>
+												<tr>
+													<th scope="col">Name</th>
+													<th scope="col">Started</th>
+													<th scope="col">Finished</th>
+													<th scope="col">Images</th>
+												</tr>
+											</thead>
+											<tbody>
+												{
+													jobs.filter((j) => j.status === 6 ).map((j) => {
+														return (
+														<tr onClick={() => this.fetchJob(j._id.$oid)}>
+															<th scope="row">{j.name}</th>
+															<td scope="row"><Moment format="DD-MM-YYYY hh:mm:ss">{j.started.$date}</Moment></td>
+															<td scope="row"><Moment format="DD-MM-YYYY hh:mm:ss">{j.ended.$date}</Moment></td>
+															<td>{j.imagesCountMin}/{j.imageCountMax}</td>
+														</tr>
+													)})
 
-                    {/* Ready div */}
-                    <div style={{display: "flex", justifyContent: "center", marginTop: "1%", marginBottom: "1%"}}>
-                        <div className="card" style={{width: "65%"}}>
-                            <div className="card-body">
-                                <h2 className="card-title">Ready</h2>
-                                {jobs.filter((j) => j.status === 2 ).length > 0  
-                                    ? 
-                                    <table className="table text-center">
-                                        <thead style={{backgroundColor: "#024a75", color: "#ffffff"}}>
-                                            <tr>
-                                                <th scope="col">Name</th>
-                                                <th scope="col">Images</th>
-                                                <th scope="col">Type</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            {
-                                                jobs.filter((j) => j.status === 2 ).map((j) => {
-                                                    return (
-                                                    <tr onClick={() => this.fetchJob(j._id.$oid)}>
-                                                        <th scope="row">{j.name}</th>
-                                                        <td scope="row">{j.imagesCount}</td>
-                                                        <td>{j.type}</td>
-                                                    </tr>
-                                                )})
-                                                
-                                            }
-                                        </tbody>
-                                    </table>
-                                    : <h4 style={{fontWeight:"500"}}>There are no jobs ready yet</h4>
-                                }
-                                
-                            </div>
-                        </div>
-                    </div>
-                    {/* Current */}
-                    <div style={{display: "flex", justifyContent: "center", marginTop: "1%", marginBottom: "1%"}}>
-                        <div className="card" style={{width: "65%"}}>
-                            <div className="card-body">
-                                <h2 className="card-title">Current</h2>
-                                {jobs.filter((j) => j.status === 3 ).length > 0 
-                                    ?
-                                    <table className="table text-center">
-                                        <thead style={{backgroundColor: "#024a75", color: "#ffffff"}}>
-                                            <tr>
-                                                <th scope="col">Name</th>
-                                                <th scope="col">Started</th>
-                                                <th scope="col">Images</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            {
-                                                jobs.filter((j) => j.status === 3 ).map((j) => {
-                                                    return (
-                                                    <tr onClick={() => this.fetchJob(j._id.$oid)}>
-                                                        <th scope="row">{j.name}</th>
-                                                        <td scope="row"><Moment format="DD-MM-YYYY hh:mm:ss">{j.started.$date}</Moment></td>
-                                                        <td>{j.images.filter((i) => i.result !== "null").length}/{j.images.length}</td>
-                                                    </tr>
-                                                )})
-                                                
-                                            }
-                                        </tbody>
-                                    </table>
-                                    :<h4 style={{fontWeight:"500"}}>There are no jobs running yet</h4>
-                                }
-                                
-                            </div>
-                        </div>
-                    </div>
-                    <div style={{display: "flex", justifyContent: "center", marginTop: "1%", marginBottom: "3%"}}>
-                        <div className="card" style={{width: "65%"}}>
-                            <div className="card-body">
-                                <h2 className="card-title">Finished</h2>
-                                {jobs.filter((j) => j.status === 6 ).length > 0
-                                    ? <table className="table text-center">
-                                        <thead style={{backgroundColor: "#024a75", color: "#ffffff"}}>
-                                            <tr>
-                                                <th scope="col">Name</th>
-                                                <th scope="col">Started</th>
-                                                <th scope="col">Finished</th>
-                                                <th scope="col">Images</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            { 
-                                                jobs.filter((j) => j.status === 6 ).map((j) => {
-                                                    return (
-                                                    <tr onClick={() => this.fetchJob(j._id.$oid)}>
-                                                        <th scope="row">{j.name}</th>
-                                                        <td scope="row"><Moment format="DD-MM-YYYY hh:mm:ss">{j.started.$date}</Moment></td>
-                                                        <td scope="row"><Moment format="DD-MM-YYYY hh:mm:ss">{j.ended.$date}</Moment></td>
-                                                        <td>{j.imagesCountMin}/{j.imageCountMax}</td>
-                                                    </tr>
-                                                )})
-                                                
-                                            }
-                                        </tbody>
-                                    </table>
-                                    : <h4 style={{fontWeight:"500"}}>There are no jobs finished yet</h4>
-                                }
-                                
-                            </div>
-                        </div>
-                    </div> 
-                </div>
-            }
-            <Footer />
-        </div>
-        );
-    }
+												}
+											</tbody>
+										</table>
+										: <h4 style={{fontWeight:"500"}}>There are no jobs finished yet</h4>
+									}
+							</div>
+						</div>
+					</div>
+				</div>
+			}
+			<Footer />
+		</div>
+		);
+	}
 
 }
 
-Jobs.propTypes = { 
-    jobs: PropTypes.object.isRequired,
-    jobs_get_all: PropTypes.func.isRequired,
-    jobs_create : PropTypes.func.isRequired,
+Jobs.propTypes = {
+	jobs: PropTypes.object.isRequired,
+	jobs_get_all: PropTypes.func.isRequired,
+	jobs_create : PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
-    jobs: state.jobs,
-    auth: state.auth
+	jobs: state.jobs,
+	auth: state.auth
 });
 
 export default compose(
-    withRouter,
-    connect (mapStateToProps, { jobs_get_all, jobs_create }) 
+	withRouter,
+	connect (mapStateToProps, { jobs_get_all, jobs_create })
 )(Jobs);
